50ea9b4f13a309da67ee0490b5d19044f5671eac
shreeman <xshreeman@gmail.com>
2026-08-29 08:39:16 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,9 +26,9 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(2) (ps,  a,   b);
+  xor #(3) (ps,  a,   b);
   and #(2) (pc1, a,   b);
-  xor #(2) (sum, cin, ps);
+  xor #(3) (sum, cin, ps);
   and #(2) (pc2, cin, ps);
   or  #(2) (cout, pc1, pc2);
 
